6af55be3a35aa61df513818f187181e341e36928
rommelsanthiago <rommel.santhiago@gmail.com>
2024-07-28 20:37:09 -0300

    Feat: Integrate Banco class into main application

--- src/main/java/com/main/MainApplication.java
+++ src/main/java/com/main/MainApplication.java
@@ -1,5 +1,7 @@
 package com.main;
 
+import java.util.ArrayList;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -15,16 +17,38 @@ public class MainApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Cliente rommel = new Cliente();
 		rommel.setNome("Rommel");
-		System.out.println("Nome do novo cliente: " + rommel.getNome());
 
 		Conta cc = new ContaCorrente(rommel);
+		System.out.println("=== Criando nova conta corrente... ===");
+		System.out.println("Conta corrente criada");
+		System.out.println("Conta número: " + cc.numero + ", Titular: " + cc.cliente.getNome());
 		Conta poupanca = new ContaPoupanca(rommel);
+		System.out.println("=== Criando nova conta poupança... ===");
+		System.out.println("Conta poupança criada");
+		System.out.println("Conta número: " + poupanca.numero + ", Titular: " + poupanca.cliente.getNome());
+
+		Banco banco = new Banco();
+        banco.setNome("Banco Digital");
+        banco.setContas(new ArrayList<>());
+        banco.getContas().add(cc);
+        banco.getContas().add(poupanca);
 
 		cc.depositar(100);
+		System.out.println("=== Deposito conta corrente ===");
+		System.out.println("Deposito de " + cc.saldo + ", para o titular: " + cc.cliente.getNome());
 		cc.sacar(50);
+		System.out.println("=== Saque conta corrente ===");
+		System.out.println("Saque de " + cc.saldo + ", para o titular: " + cc.cliente.getNome());
 		cc.transferir(100, poupanca);
+		System.out.println("=== Transferencia conta corrente para poupança ===");
+		System.out.println("Transferencia de " + cc.saldo + ", para o titular: " + poupanca.cliente.getNome());
 		
 		cc.imprimirExtrato();
 		poupanca.imprimirExtrato();
+
+		System.out.println("=== Contas no " + banco.getNome() + " ===");
+        for (Conta conta : banco.getContas()) {
+            System.out.println("Conta número: " + conta.getNumero() + ", Saldo: " + conta.getSaldo());
+        }
 	}
 }
