e7f2d64a2f55addb10200a7a4ae77676b34d93d4
rommelsanthiago <rommel.santhiago@gmail.com>
2024-07-28 19:35:04 -0300

    Feat: Implement client model

--- src/main/java/com/main/MainApplication.java
+++ src/main/java/com/main/MainApplication.java
@@ -1,13 +1,20 @@
 package com.main;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Cliente rommel = new Cliente();
+		rommel.setNome("Rommel");
+		System.out.println("Nome do novo cliente: " + rommel.getNome());
+	}
 }
