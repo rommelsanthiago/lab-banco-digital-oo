75617da5b2b9e6e03f2bc24120b9fcaf248f7848
rommelsanthiago <rommel.santhiago@gmail.com>
2024-07-28 21:31:11 -0300

    Feat: Generate and add Javadoc documentation

--- src/main/java/com/main/Conta.java
+++ src/main/java/com/main/Conta.java
@@ -1,5 +1,13 @@
 package com.main;
 
+/**
+ * Representa uma conta bancária.
+ * 
+ * <p>Esta classe permite criar uma conta bancária e realizar operações como depósito, transferência e saque.</p>
+ * 
+ * @author Rommel
+ * @version 1.0
+ */
 public abstract class Conta {
     private static final int AGENCIA_PADRAO = 1;
     private static int SEQUENCIAL = 1;
@@ -9,37 +17,76 @@ public abstract class Conta {
     protected double saldo;
     protected Cliente cliente;
 
+    /**
+     * Cria uma nova conta bancária para o cliente especificado.
+     * 
+     * @param cliente o cliente associado à conta
+     */
     public Conta(Cliente cliente) {
         this.agencia = AGENCIA_PADRAO;
         this.numero = SEQUENCIAL++;
         this.cliente = cliente;
     }
 
+    /**
+     * Realiza um depósito na conta.
+     * 
+     * @param valor o valor a ser depositado
+     */
     public void depositar(double valor) {
         saldo += valor;
     }
 
+    /**
+     * Realiza um saque da conta.
+     * 
+     * @param valor o valor a ser sacado
+     */
     public void sacar(double valor) {
         saldo -= valor;
     }
 
+    /**
+     * Transfere um valor para outra conta.
+     * 
+     * @param valor o valor a ser transferido
+     * @param contaDestino a conta para a qual o valor será transferido
+     */
     public void transferir(double valor, Conta contaDestino) {
         this.sacar(valor);
         contaDestino.depositar(valor);
     }
 
+    /**
+     * Retorna o número da agência da conta.
+     * 
+     * @return o número da agência
+     */
     public int getAgencia() {
         return agencia;
     }
 
+    /**
+     * Retorna o número da conta.
+     * 
+     * @return o número da conta
+     */
     public int getNumero() {
         return numero;
     }
 
+    /**
+     * Retorna o saldo atual da conta.
+     * 
+     * @return o saldo da conta
+     */
     public double getSaldo() {
         return saldo;
     }
 
+    /**
+     * Imprime informações comuns da conta, como titular, agência, número e saldo.
+     */
     protected void imprimirInfosComuns() {
         System.out.println(String.format("Titular: %s", this.cliente.getNome()));
         System.out.println(String.format("Agência: %d", this.agencia));
@@ -47,5 +94,8 @@ public abstract class Conta {
         System.out.println(String.format("Saldo: %.2f", this.saldo));
     }
 
+    /**
+     * Método abstrato que deve ser implementado pelas subclasses para imprimir o extrato da conta.
+     */
     public abstract void imprimirExtrato();
 }
